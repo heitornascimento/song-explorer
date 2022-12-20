9043a3632bd2ea5155fd722517c6327c55ff559b
Heitor Souza <heitor.nascimento@n26brasil.com>
2022-12-19 22:07:36 -0300

    Android project creation

---
+++ app/src/main/java/com/demo/songexplorer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.songexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
